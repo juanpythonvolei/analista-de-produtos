da98d3b029b68ee36bdb8741a95003f9a50c5ea0
juanpythonvolei <juanzsalca@outlook.com>
2024-10-06 19:34:24 -0300

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from analista import *
-produto = st.chat_message("user")
+produto = st.text_input(label='',placeholder='Insira o produto desejado')
 if produto:
-    produto.write(produto)
+    user = st.chat_message("user")
+    user.write(produto)
     bot = st.chat_message("ai")
     bot.write(resposta(acessar_links(pesquisar_produto(produto))))
