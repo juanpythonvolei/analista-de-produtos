e428ed223d9d30450587f1299f5477497a2a5015
juanpythonvolei <juanzsalca@outlook.com>
2024-10-10 23:14:04 -0300

    Update analista.py

--- analista.py
+++ analista.py
@@ -31,7 +31,7 @@ def resposta(lista_paginas):
     for pagina in lista_paginas:
         try:
             resposta_infos = analisar(f'''
-            Por favor, analise esse html referente a um produto em um site e me retorne as seguintes informações: Preço do produto mais vantajoso, vendedor, nome do produto, resumo das informaões técnicas,link do produto e nome do site em que está o item.Apenas o necessário mesmo. Me retorne essas informações em um texto, no quais os tópicos que te mostrei devem estar saparados por esse caracter"....//".Se você não puder indentificar esses tópicos no html por conta de resticões de acesso, por favor, não o analise''',pagina
+            Por favor, analise esse html referente a um produto em um site e me retorne as seguintes informações: Preço do produto mais vantajoso, vendedor, nome do produto, resumo das informaões técnicas,link do produto e nome do site em que está o item.Apenas o necessário mesmo. Me retorne essas informações em um texto, no quais os tópicos que te mostrei devem estar saparados por esse caracter"....//".Se você não puder indentificar esses tópicos no html por conta de resticões de acesso, por favor, não o analise''',str(pagina)
         
             )
             lista_infos.append(f'{resposta_infos}')
@@ -39,6 +39,6 @@ def resposta(lista_paginas):
         except:
             pass
     try:
-        return analisar(f"Você é um analisador de produtos e sua função é me ajudar a selecionar o melhor local de compra e o melhor produto a ser selecionado. Te apresentarei uma lista de informações referentes a um produto, vindas de sites e vendedores diferentes. Por favor, retorne para mim a sua avaliação  de qual é o produto mais vantajoso o vendedor desses produto e o site no qual ele está hospedado bem como seu link de acesso. Além disso, elabore uma tabela com os demais produtos e vendedores, elencando-os do mais barato para o mais caro.",lista_infos)
+        return analisar(f"Você é um analisador de produtos e sua função é me ajudar a selecionar o melhor local de compra e o melhor produto a ser selecionado. Te apresentarei uma lista de informações referentes a um produto, vindas de sites e vendedores diferentes. Por favor, retorne para mim a sua avaliação  de qual é o produto mais vantajoso o vendedor desses produto e o site no qual ele está hospedado bem como seu link de acesso. Além disso, elabore uma tabela com os demais produtos e vendedores, elencando-os do mais barato para o mais caro.",str(lista_infos))
     except:
         print("Não foi possível realizar a análise dos produtos")
