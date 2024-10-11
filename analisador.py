6a9a1d43416ea86bf0e76367d2912104b6e9ca45
juanpythonvolei <juanzsalca@outlook.com>
2024-10-10 22:58:21 -0300

    Update analisador.py

--- analisador.py
+++ analisador.py
@@ -1,9 +1,9 @@
 import google.generativeai as genai
 import streamlit as st
-def analisar(pergunta):
+def analisar(pergunta,conteudo):
     
-    genai.configure(api_key=st.secrets["ia"])
+    genai.configure(api_key='AIzaSyB2uaEtcP8T2_Fy6bhmXC3828qysZEqjNQ')
     model = genai.GenerativeModel('gemini-1.5-flash')
-    chat = model.start_chat(history=[])
+    chat = model.start_chat(history=[{"role": "user", "parts": [{"text": conteudo}]}])
     response = chat.send_message(pergunta)
     return response.text
