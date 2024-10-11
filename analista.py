8d6158e7c1031f12809ab7521efd6aa88d229c81
juanpythonvolei <juanzsalca@outlook.com>
2024-10-10 23:01:12 -0300

    Update analista.py

--- analista.py
+++ analista.py
@@ -10,7 +10,7 @@ def pesquisar_produto(produto):
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
 
-        lista_links = (analisar(f"Por favor me retorne uma lista com os links desse html, que levem a sites de compra como: tudocelular,amazon,mercadolivre etc... . Retorne apenas a lista python, sem as aspas externa e com os links separados por virgulas.Apenas retorne os links que estejam relacionados com o produto {produto}, por favor. Segue o html:{soup}"))
+        lista_links = (analisar(f"Por favor me retorne uma lista com os links desse html, que levem a sites de compra como: tudocelular,amazon,mercadolivre etc... . Retorne apenas a lista python, sem as aspas externa e com os links separados por virgulas.Apenas retorne os links que estejam relacionados com o produto {produto}",soup))
 
         links = lista_links.split(',')
         return links
@@ -31,7 +31,7 @@ def resposta(lista_paginas):
     for pagina in lista_paginas:
         try:
             resposta_infos = analisar(f'''
-            Por favor, analise esse html referente a um produto em um site e me retorne as seguintes informações: Preço do produto mais vantajoso, vendedor, nome do produto, resumo das informaões técnicas,link do produto e nome do site em que está o item.Apenas o necessário mesmo. Me retorne essas informações em um texto, no quais os tópicos que te mostrei devem estar saparados por esse caracter"....//".Se você não puder indentificar esses tópicos no html por conta de resticões de acesso, por favor, não o analise.Segue o hmtl: {pagina}'''
+            Por favor, analise esse html referente a um produto em um site e me retorne as seguintes informações: Preço do produto mais vantajoso, vendedor, nome do produto, resumo das informaões técnicas,link do produto e nome do site em que está o item.Apenas o necessário mesmo. Me retorne essas informações em um texto, no quais os tópicos que te mostrei devem estar saparados por esse caracter"....//".Se você não puder indentificar esses tópicos no html por conta de resticões de acesso, por favor, não o analise''',pagina
         
             )
             lista_infos.append(f'{resposta_infos}')
@@ -39,6 +39,6 @@ def resposta(lista_paginas):
         except:
             pass
     try:
-        return analisar(f"Você é um analisador de produtos e sua função é me ajudar a selecionar o melhor local de compra e o melhor produto a ser selecionado. Te apresentarei uma lista de informações referentes a um produto, vindas de sites e vendedores diferentes. Por favor, retorne para mim a sua avaliação  de qual é o produto mais vantajoso o vendedor desses produto e o site no qual ele está hospedado bem como seu link de acesso. Além disso, elabore uma tabela com os demais produtos e vendedores, elencando-os do mais barato para o mais caro. Segue a lista: {lista_infos}")
+        return analisar(f"Você é um analisador de produtos e sua função é me ajudar a selecionar o melhor local de compra e o melhor produto a ser selecionado. Te apresentarei uma lista de informações referentes a um produto, vindas de sites e vendedores diferentes. Por favor, retorne para mim a sua avaliação  de qual é o produto mais vantajoso o vendedor desses produto e o site no qual ele está hospedado bem como seu link de acesso. Além disso, elabore uma tabela com os demais produtos e vendedores, elencando-os do mais barato para o mais caro.",lista_infos)
     except:
         print("Não foi possível realizar a análise dos produtos")
