6a2affad150c1259f328212d7bd374cbee633e40
juanpythonvolei <juanzsalca@outlook.com>
2024-10-10 23:42:18 -0300

    Update analista.py

--- analista.py
+++ analista.py
@@ -8,8 +8,8 @@ def pesquisar_produto(produto):
         pesquisa = produto.split(" ")
         for item in pesquisa:
                 url_base += f"+{item}"
-        
-        url = f"https://www.bing.com/search?q={produto[0]}+{produto[1]}"
+        pesquisa2 = produto.split("")
+        url = f"https://www.bing.com/search?q={produto2[0]}+{produto2[1]}"
         #url_ia = analisar(f"Por favor, crie uma url de pesquisa para o navegador bing, a qual contenha o conteudo desse input: {produto}.Retorne apenas o link, sem aspas, ou outros caracteres." )
         #url = url_ia.replace("```",'').strip()
         response = requests.get(url_base)
